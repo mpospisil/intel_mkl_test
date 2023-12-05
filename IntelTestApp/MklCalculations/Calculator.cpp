1611d3e6f70ec32f361db8cca743a03a2e5353fa
Martin Pospisil <martin.pospisil@ideastatica.com>
2023-12-05 14:33:40 +0100

    adding intel's dlls

--- IntelTestApp/MklCalculations/Calculator.cpp
+++ IntelTestApp/MklCalculations/Calculator.cpp
@@ -1,10 +1,17 @@
 #include "pch.h"
 #include "Calculator.h"
 #include <iostream>
-
+#include "..\intel\omp.h"
 
 int Calculator::Calculate(int)
 {
-	std::cout << "Caluculate\n";
+	std::cout << "Calculate\n";
+
+	void* p = kmp_malloc(100);
+	int num;
+	omp_get_partition_place_nums(&num);
+
+	int device = omp_get_initial_device();
+	kmp_free(p);
 	return 100;
 }
